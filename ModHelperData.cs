f6fd43eb2fcd6b6508e986aaf4c2f2aede3436ff
eh44 <133456551+eh44@users.noreply.github.com>
2023-10-16 19:29:04 -0600

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -7,11 +7,12 @@ public static class ModHelperData
     public const string Name = "Boss Rounds With Speed/Health Multipliers";
 
     public const string Description =
+        "Forked from doombubbles' boss rounds\n\n" + 
         "Adds Round Sets for all Bosses and their Elite versions chooseable in the Round Set Changer, " +
         "allowing you to play any boss on any map at any time. " +
         "Sandbox also works, allowing you to test Bosses by sending their corresponding rounds.\n\n" +
-        "You can change the health of a boss by pressing f7 and the speed of a boss by pressing f8."
-        "This needs to be done before entering a game.\n\n"
+        "You can change the health of a boss by pressing f7 and the speed of a boss by pressing f8."+
+        "This needs to be done before entering a game.\n\n" +
         "You DO NOT receive the usual boss rewards / trophies for playing, " +
         "instead you just get a nice chunk of extra Monkey Money.\n\n" +
         "Saving and loading on maps is fully functional, but know that you'll need to have " +
