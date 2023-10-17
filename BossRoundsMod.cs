4dc41fb7319f9f428a56c7fa8cca08d046f6f5ed
eh44 <133456551+eh44@users.noreply.github.com>
2023-10-16 18:46:00 -0600

    Update BossRoundsMod.cs
    Added speed/health multiplier functionality.

--- BossRoundsMod.cs
+++ BossRoundsMod.cs
@@ -1,19 +1,26 @@
 using System;
 using BossRounds;
 using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
 using BTD_Mod_Helper.Extensions;
 using BTD_Mod_Helper.UI.Modded;
 using Il2Cpp;
 using Il2CppAssets.Scripts.Data;
+using Il2CppAssets.Scripts.Data.Gameplay;
 using Il2CppAssets.Scripts.Models;
+using Il2CppAssets.Scripts.Models.Bloons;
 using Il2CppAssets.Scripts.Models.ServerEvents;
 using Il2CppAssets.Scripts.Unity;
 using Il2CppAssets.Scripts.Unity.Menu;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using Il2CppAssets.Scripts.Unity.UI_New.Main.DifficultySelect;
 using Il2CppAssets.Scripts.Unity.UI_New.Main.ModeSelect;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using Il2CppSystem.Threading.Tasks;
 using MelonLoader;
+using UnityEngine;
+
 [assembly: MelonInfo(typeof(BossRoundsMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
@@ -21,6 +28,9 @@ namespace BossRounds;
 
 public class BossRoundsMod : BloonsTD6Mod
 {
+    
+    public static float BossHealth = 1;
+    public static float BossSpeed = 1;
     public const string EventId = nameof(BossRoundsMod);
     public const string BossTypeKey = "BossRounds-BossType";
     public const string IsEliteKey = "BossRounds-IsElite";
@@ -29,19 +39,56 @@ public class BossRoundsMod : BloonsTD6Mod
     public const int BaseMonkeyMoneyBonus = 300;
     public const float MapModeMonkeyMoneyMult = .5f;
     public const float EliteMonkeyMoneyMult = 2;
-
+    private static readonly ModSettingHotkey BossHealthCustom = new(KeyCode.F7)
+    {
+        displayName = "Custom Boss Health HotKey"
+    };
+    private static readonly ModSettingHotkey BossSpeedCustom = new(KeyCode.F8)
+    {
+        displayName = "Custom Boss Speed HotKey"
+    };
     public BossRoundSet? SelectedSet { get; private set; }
 
     public override void OnNewGameModel(GameModel gameModel)
     {
+       
         if (InGameData.CurrentGame.gameEventId == EventId)
-        {
+        {   
             gameModel.endRound = 140;
         }
     }
 
     public override void OnUpdate()
     {
+        if (BossHealthCustom.JustPressed())
+        {
+            PopupScreen.instance.ShowSetValuePopup("Custom Boss Health",
+                "Sets the Boss Health to the specified percentage of health",
+                new Action<int>(i =>
+                {
+                    if (i <= 1)
+                    {
+                        i = 1;
+                    }
+
+                    BossHealth = ((float)i)/100;
+                }), (int) BossHealth);
+        }
+        if (BossSpeedCustom.JustPressed())
+        {
+            PopupScreen.instance.ShowSetValuePopup("Custom Boss Speed",
+                "Sets the Boss speed to the specified percentage of speed",
+                new Action<int>(i =>
+                {
+                    if (i <= 1)
+                    {
+                        i = 1;
+                    }
+
+                    BossSpeed = i / 100;
+                }), (int) BossSpeed);
+        }
+
         if (MenuManager.instance == null) return;
 
         var updateScreens = false;
